ca4c55faf227871e070ca4ddd31a371a37b55e7f
27Ivan0vS <IvanovSergey.94.28.@gmail.com>
2022-06-25 19:53:35 +0300

    feat: implement reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    const stringN = String(Math.abs(n));
+    let newString = '';
+
+    for (let item of stringN) {
+        newString = item + newString
+    }
+    return +newString;
 }
